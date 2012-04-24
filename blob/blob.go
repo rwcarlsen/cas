079542aa081be41f51da4c2f91f72c5352637618
Robert Carlsen <rwcarlsen@gmail.com>
2012-04-24 18:15:02 -0500

    more playing.

--- blob/blob.go
+++ blob/blob.go
@@ -4,23 +4,41 @@ package blob
 import (
   "crypto/sha256"
   "hash"
+  "encoding/hex"
 )
 
 type blob struct {
-  h hash.Hash
+  hashFunc hash.Hash
+  hashName string
+  content []byte
 }
 
 func New() *blob {
   h := sha256.New()
-  return &blob{h:h}
+  name := "sha256"
+  return &blob{hashFunc:h, hashName: name}
 }
 
-func (b *blob) Write(data []byte) {
-  b.h.Write(data)
+func (b *blob) Write(data []byte) (n int, err error) {
+  n = len(data)
+  err = nil
+
+  b.content = append(b.content, data...)
+  b.hashFunc.Write(data)
+
+  return
 }
 
 func (b *blob) Sum() []byte {
-  sum := b.h.Sum([]byte{})
+  sum := b.hashFunc.Sum([]byte{})
   return sum
+}
+
+func (b *blob) FileName() string {
+  return b.hashName + "-" + hex.EncodeToString(b.Sum())
+}
+
+func (b *blob) Content() []byte {
+  return b.content
 }
 
--- main.go
+++ main.go
@@ -4,12 +4,11 @@ package main
 import (
   "fmt"
   "./blob"
-  "encoding/hex"
 )
 
 func main() {
   b := blob.New()
   b.Write([]byte("hello"))
 
-  fmt.Println(hex.EncodeToString(b.Sum()))
+  fmt.Println(b.FileName())
 }
