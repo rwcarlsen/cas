324b77e55ad1dface9336250a25a08b83ab5a912
Robert Carlsen <rwcarlsen@gmail.com>
2012-06-19 17:20:25 -0500

    bug fix

--- blobdb/blobdb.go
+++ blobdb/blobdb.go
@@ -49,7 +49,7 @@ func (db *dbase) Get(ref string) (b *blob.Blob, err error) {
   }
 
   hash, sum := blobRefParts(ref)
-  b = blob.Raw(data)
+  b = blob.FromContent(data)
   b.Hash = hash
 
   err = verifyBlob(sum, b)
