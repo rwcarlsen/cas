e11978290dc280c98b8892a3fe6f6c30b4b5c083
Robert Carlsen <rwcarlsen@gmail.com>
2012-07-20 10:08:24 -0500

    using templates to populate links to installed apps on appserver

--- appserver/server.go
+++ appserver/server.go
@@ -5,29 +5,43 @@ import (
   "fmt"
   "net/http"
   "strings"
+  "sort"
+  "html/template"
   "github.com/rwcarlsen/cas/util"
   "github.com/rwcarlsen/cas/app"
   "github.com/rwcarlsen/cas/appserver/notedrop"
   "github.com/rwcarlsen/cas/appserver/fupload"
-  "github.com/rwcarlsen/cas/timeindex"
-  "time"
 )
 
 var defaultContext *app.Context = &app.Context{"http://rwc-server.dyndns.org:7777", "robert", "password"}
 
 var handlers map[string]app.HandleFunc
 
+// add new apps by listing them here in this init func
 func init() {
-  handlers = make(map[string]app.HandleFunc)
+  handlers = map[string]app.HandleFunc{}
   handlers["notedrop"] = notedrop.Handle
   handlers["fupload"] = fupload.Handle
+}
+
+var tmpl *template.Template
+var applist []string
+func init() {
+  tmpl = template.Must(template.ParseFiles("index.tmpl", "applinks.tmpl"))
+
+  applist = make([]string, 0)
+  for name, _ := range handlers {
+    applist = append(applist, name)
+  }
+
+  sort.Strings(applist)
 }
 
 func main() {
   http.HandleFunc("/", handler)
 
   fmt.Println("Starting http server...")
-  err = http.ListenAndServe("0.0.0.0:8888", nil)
+  err := http.ListenAndServe("0.0.0.0:8888", nil)
 
   if err != nil {
     fmt.Println(err)
@@ -42,12 +56,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
   base := strings.Split(pth, "/")[0]
 
   if base == "" {
-    err := util.LoadStatic("index.html", w)
+    err := tmpl.Execute(w, applist)
     util.Check(err)
   } else if _, ok := handlers[base]; ok {
     handlers[base](defaultContext, w, r)
   } else {
-    fmt.Println("loading static file", pth)
     err := util.LoadStatic(pth, w)
     util.Check(err)
   }
