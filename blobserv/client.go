6bf04f301e88d21fd083aec995b225f07c1a0682
Robert Carlsen <rwcarlsen@gmail.com>
2013-06-26 11:50:49 -0500

    working on updating blobserv server and client to use reworked packages.  Still need error handling and to work out compiler issues.

--- blobserv/client.go
+++ blobserv/client.go
@@ -1,207 +1,92 @@
-
 package blobserv
 
 import (
-  "bytes"
-  "time"
-  "strconv"
-  "mime/multipart"
-  "encoding/json"
-  "io/ioutil"
-  "net/http"
-  "crypto/tls"
-  "errors"
-  "github.com/rwcarlsen/cas/blob"
-  "github.com/rwcarlsen/cas/blobserv/timeindex"
-  "github.com/rwcarlsen/cas/blobserv/objindex"
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 type Client struct {
-  Host string
-  User string
-  Pass string
+	Host string
+	User string
+	Pass string
 }
 
 func (c *Client) setAuth(r *http.Request) {
-  if c.User + c.Pass != "" {
-    r.SetBasicAuth(c.User, c.Pass)
-  }
+	if c.User+c.Pass != "" {
+		r.SetBasicAuth(c.User, c.Pass)
+	}
 }
 
 func (c *Client) GetBlob(ref string) (*blob.Blob, error) {
-  data, err := c.GetBlobContent(ref)
-  if err != nil {
-    return nil, err
-  }
-  return blob.NewRaw(data), nil
-}
-
-func (c *Client) Dial() error {
-  r, err := http.NewRequest("GET", c.Host, nil)
-  if err != nil {
-    return err
-  }
-  r.URL.Path = "/ref/foo"
-  _, err = getClient().Do(r)
-  return err
+	data, err := c.GetBlobContent(ref)
+	if err != nil {
+		return nil, err
+	}
+	return blob.NewRaw(data), nil
 }
 
 func getClient() *http.Client {
-  // allows me to use encryption certificate that is not signed by a
-  // verified authority
-  config := &tls.Config{InsecureSkipVerify: true}
-  return &http.Client{
-    Transport: &http.Transport{TLSClientConfig: config, Proxy: http.ProxyFromEnvironment},
-  }
+	// allows me to use encryption certificate that is not signed by a
+	// verified authority
+	config := &tls.Config{InsecureSkipVerify: true}
+	return &http.Client{
+		Transport: &http.Transport{TLSClientConfig: config, Proxy: http.ProxyFromEnvironment},
+	}
 }
 
-func (c *Client) GetBlobContent(ref string) ([]byte, error) {
-  r, err := http.NewRequest("GET", c.Host, nil)
-  if err != nil {
-    return nil, err
-  }
-
-  r.URL.Path = "/ref/" + ref
-  c.setAuth(r)
-
-  resp, err := getClient().Do(r)
-  if err != nil {
-    return nil, err
-  }
-
-  status := resp.Header.Get(ActionStatus)
-  if status == ActionFailed {
-    return nil, errors.New("blobserv: blob retrieval failed")
-  }
-
-  content, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return nil, err
-  }
-
-  resp.Body.Close()
-  return content, nil
-}
-
-func (c *Client) ReconstituteFile(ref string) (m *blob.Meta, content []byte, err error) {
-  b, err := c.GetBlob(ref)
-  if err != nil {
-    return nil, nil, err
-  }
-
-  m = &blob.Meta{}
-  err = blob.Unmarshal(b, m)
-  if err != nil {
-    return nil, nil, err
-  }
-
-  chunks := []*blob.Blob{}
-  for _, ref := range m.ContentRefs {
-    b, err := c.GetBlob(ref)
-    if err != nil {
-      return nil, nil, err
-    }
-    chunks = append(chunks, b)
-  }
-
-  return m, blob.Reconstitute(chunks...), nil
-}
-
-func (c *Client) PutBlob(b *blob.Blob) error {
-  r, err := http.NewRequest("POST", c.Host, bytes.NewBuffer(b.Content()))
-  if err != nil {
-    return err
-  }
-
-  r.URL.Path = "/put/"
-  c.setAuth(r)
-  resp, err := getClient().Do(r)
-  if err != nil {
-    return err
-  }
-
-  status := resp.Header.Get(ActionStatus)
-  if status == ActionFailed {
-    return errors.New("blobserv: blob posting failed")
-  }
-
-  return nil
-}
-
-func (c *Client) IndexBlobs(name string, nBlobs int, params interface{}) ([]*blob.Blob, error) {
-  data, err := json.Marshal(params)
-  if err != nil {
-    return nil, err
-  }
-
-  r, err := http.NewRequest("POST", c.Host, bytes.NewBuffer(data))
-  if err != nil {
-    return nil, err
-  }
-
-  r.URL.Path = "/index/"
-  r.Header.Set(IndexField, name)
-  r.Header.Set(ResultCountField, strconv.Itoa(nBlobs))
-  c.setAuth(r)
-
-  resp, err := getClient().Do(r)
-  if err != nil {
-    return nil, err
-  }
-
-  boundary := resp.Header.Get(BoundaryField)
-  mr := multipart.NewReader(resp.Body, boundary)
-
-  blobs := []*blob.Blob{}
-	for {
-		part, err := mr.NextPart()
-    if err != nil {
-      break
-    }
-
-    if part.FileName() == "" {
-      continue
-    }
-
-    data, err := ioutil.ReadAll(part)
-    if err != nil {
-      return nil, err
-    }
-
-    blobs = append(blobs, blob.NewRaw(data))
+func (c *Client) GetBlob(ref string) ([]byte, error) {
+	r, err := http.NewRequest("GET", c.Host, nil)
+	if err != nil {
+		return nil, err
 	}
 
-  if len(blobs) == 0 {
-    return nil, errors.New("blobserv: no blobs for that index query")
-  }
+	r.URL.Path = "/ref/" + ref
+	c.setAuth(r)
 
-  return blobs, nil
+	resp, err := getClient().Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	status := resp.Header.Get(ActionStatus)
+	if status == ActionFailed {
+		return nil, errors.New("blobserv: blob retrieval failed")
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	resp.Body.Close()
+	return content, nil
 }
 
-func (c *Client) BlobsBackward(t time.Time, n, nskip int) ([]*blob.Blob, error) {
-  indReq := timeindex.Request{
-    Time: t,
-    Dir:timeindex.Backward,
-    SkipN: nskip,
-  }
-  return c.IndexBlobs("time", n, &indReq)
-}
+func (c *Client) SendBlob(blobs ...[]byte) error {
+	r, err := http.NewRequest("POST", c.Host, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
 
-func (c *Client) BlobsForward(t time.Time, n, nskip int) ([]*blob.Blob, error) {
-  indReq := timeindex.Request{
-    Time: t,
-    Dir:timeindex.Forward,
-    SkipN: nskip,
-  }
-  return c.IndexBlobs("time", n, &indReq)
-}
+	r.URL.Path = "/put/"
+	c.setAuth(r)
+	resp, err := getClient().Do(r)
+	if err != nil {
+		return err
+	}
 
-func (c *Client) ObjectTip(objref string) (*blob.Blob, error) {
-  objReq := objindex.Request{ObjectRef:objref}
-  blobs, err := c.IndexBlobs("object", 1, objReq)
-  if err != nil {
-    return nil, err
-  }
-  return blobs[0], nil
+	status := resp.Header.Get(ActionStatus)
+	if status == ActionFailed {
+		return errors.New("blobserv: blob posting failed")
+	}
+
+	return nil
 }
 
--- blobserv/server.go
+++ blobserv/server.go
@@ -1,287 +1,144 @@
-
 package blobserv
 
 import (
-  "fmt"
-  "sort"
-  "time"
-  "strconv"
-  "errors"
-  "path"
-  "io/ioutil"
-  "net/http"
-  "mime/multipart"
-  "github.com/rwcarlsen/cas/blob"
-  "github.com/rwcarlsen/cas/blobdb"
-  "github.com/rwcarlsen/cas/blobserv/index"
-  "github.com/rwcarlsen/cas/auth"
-  "github.com/rwcarlsen/cas/util"
-  "github.com/rwcarlsen/cas/blobserv/timeindex"
-  "github.com/rwcarlsen/cas/blobserv/objindex"
+	"errors"
+	"fmt"
+	"github.com/rwcarlsen/cas/auth"
+	"github.com/rwcarlsen/cas/blobdb"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"path"
+	"sort"
+	"strconv"
+	"time"
 )
 
 const (
-  DefaultDb = "~/.rcas"
-  DefaultAddr = "0.0.0.0:7777"
-  defaultReadTimeout = 60 * time.Second
-  defaultWriteTimeout = 60 * time.Second
-  defaultHeaderMax = 1 << 20 // 1 Mb
+	DefaultDb           = "~/.rcas"
+	DefaultAddr         = "0.0.0.0:7777"
+	defaultReadTimeout  = 60 * time.Second
+	defaultWriteTimeout = 60 * time.Second
+	defaultHeaderMax    = 1 << 20 // 1 Mb
 )
 
 const (
-  ActionStatus = "Action-Status"
-  ActionFailed = "blob get/put failed"
-  ActionSuccess = "blob get/put succeeded"
+	ActionStatus  = "Action-Status"
+	ActionFailed  = "blob get/put failed"
+	ActionSuccess = "blob get/put succeeded"
 )
 
 const (
-  GetField = "Blob-Ref"
-  IndexField = "Index-Name"
-  ResultCountField = "Num-Index-Results"
-  BoundaryField = "Blob-Boundary"
+	GetField         = "Blob-Ref"
+	ResultCountField = "Num-Index-Results"
+	BoundaryField    = "Blob-Boundary"
 )
 
-var (
-  DupIndexNameErr = errors.New("blobserv: index name already exists")
-)
-
-func ListenAndServe(addr string, dbPath string) error {
-  bs := configServ(addr, dbPath)
-  return bs.ListenAndServe()
+func ListenAndServe(addr string, db blobdb.Interface) error {
+	bs := configServ(addr, dbPath)
+	return bs.ListenAndServe()
 }
 
-func ListenAndServeTLS(addr, dbPath string, certFile, keyFile string) error {
-  bs := configServ(addr, dbPath)
-  return bs.ListenAndServeTLS(certFile, keyFile)
+func ListenAndServeTLS(addr, db blobdb.Interface, certFile, keyFile string) error {
+	bs := configServ(addr, dbPath)
+	return bs.ListenAndServeTLS(certFile, keyFile)
 }
 
-func configServ(addr, dbPath string) *Server {
-  db, _ := blobdb.New(dbPath)
-  tInd := timeindex.New()
-  oInd := objindex.New()
-  for b := range db.Walk() {
-    tInd.Notify(b)
-    oInd.Notify(b)
-  }
-  sort.Sort(tInd)
-  oInd.Sort()
-
-  serv := defaultHttpServer()
-  serv.Addr = addr
-  bs := &Server{Db: db, Serv: serv}
-  bs.AddIndex("time", tInd)
-  bs.AddIndex("object", oInd)
-  return bs
+func configServ(addr, db blobdb.Interface) *Server {
+	serv := defaultHttpServer()
+	serv.Addr = addr
+	return &Server{Db: db, Serv: serv}
 }
 
 func defaultHttpServer() *http.Server {
-  return &http.Server{
-    Addr: DefaultAddr,
-    ReadTimeout: defaultReadTimeout,
-    WriteTimeout: defaultWriteTimeout,
-    MaxHeaderBytes: defaultHeaderMax,
-  }
+	return &http.Server{
+		Addr:           DefaultAddr,
+		ReadTimeout:    defaultReadTimeout,
+		WriteTimeout:   defaultWriteTimeout,
+		MaxHeaderBytes: defaultHeaderMax,
+	}
 }
 
 type Server struct {
-  Db *blobdb.Dbase
-  Serv *http.Server
-  inds map[string]index.Index
+	Db        *blobdb.Interface
+	Serv      *http.Server
+	listeners []*Client
 }
 
-func (bs *Server) AddIndex(name string, ind index.Index) error {
-  if bs.inds == nil {
-    bs.inds = make(map[string]index.Index, 0)
-  } else {
-    if _, ok := bs.inds[name]; ok {
-      return DupIndexNameErr
-    }
-  }
-
-  bs.inds[name] = ind
-  return nil
+func (bs *Server) AddListener(c *Client) {
+	listeners = append(listeners, addr)
 }
 
-func (bs *Server) notify(blobs ...*blob.Blob) {
-  for _, ind := range bs.inds {
-    ind.Notify(blobs...)
-  }
-}
-
-func (bs *Server) prepareListen() {
-  if bs.inds == nil {
-    bs.inds = make(map[string]index.Index, 0)
-  }
-
-  if bs.Db == nil {
-    bs.Db, _ = blobdb.New(DefaultDb)
-  }
-
-  if bs.Serv == nil {
-    bs.Serv = defaultHttpServer()
-  }
-
-  http.Handle("/", &defHandler{})
-  http.Handle("/ref/", auth.Handler{&getHandler{bs: bs}})
-  http.Handle("/put/", auth.Handler{&putHandler{bs: bs}})
-  http.Handle("/index/", auth.Handler{&indexHandler{bs: bs}})
+func (bs *Server) notifyListeners(blob []byte) {
+	for _, c := range bs.listeners {
+		err := c.SendBlob(blob)
+	}
 }
 
 func (bs *Server) ListenAndServe() error {
-  bs.prepareListen()
-  return bs.Serv.ListenAndServe()
+	bs.prepareListen()
+	return bs.Serv.ListenAndServe()
 }
 
 func (bs *Server) ListenAndServeTLS(certFile, keyFile string) error {
-  bs.prepareListen()
-  return bs.Serv.ListenAndServeTLS(certFile, keyFile)
+	bs.prepareListen()
+	return bs.Serv.ListenAndServeTLS(certFile, keyFile)
 }
 
-type defHandler struct {}
+func (bs *Server) prepareListen() {
+	if bs.Serv == nil {
+		bs.Serv = defaultHttpServer()
+	}
 
-func (h *defHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-  w.Write([]byte("Page doesn't exist"))
+	r := mux.NewRouter()
+	r.HandleFunc("/", defHandler)
+	r.Handle("/get/{ref:.*}", auth.Handler{&getHandler{bs: bs}})
+	r.Handle("/put/", auth.Handler{&putHandler{bs: bs}})
+	r.Handle("/enumerate/after:{after:.*}/limit:{limit:[0-9]*}", auth.Handler{&enumHandler{bs: bs}})
+	bs.Serv.Handler = r
+}
+
+func defHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("rwc cas blobserver"))
 }
 
 type getHandler struct {
-  bs *Server
+	bs *Server
 }
 
 func (h *getHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-  defer func() {
-    if r := recover(); r != nil {
-      w.Header().Set(ActionStatus, ActionFailed)
-      fmt.Println("blob post failed: ", r)
-    }
-  }()
+	vars := mux.Vars(r)
+	vars["ref"]
 
-  ref := path.Base(req.URL.Path)
-  b, err := h.bs.Db.Get(ref)
-  util.Check(err)
-
-  w.Header().Set(ActionStatus, ActionSuccess)
-  w.Write(b.Content())
-  fmt.Println("successful retrieval")
+	r, err := h.bs.Db.Get(vars["ref"])
+	_, err = io.Copy(w, r)
 }
 
-// Unauthorized checks for and handles cases where authentication can occur via
-// share blobs.
 func (h *getHandler) Unauthorized(w http.ResponseWriter, req *http.Request) {
-  defer func() {
-    if r := recover(); r != nil {
-      w.Header().Set(ActionStatus, ActionFailed)
-      fmt.Println("blob post failed: ", r)
-    }
-  }()
-
-  shareRef := req.FormValue("via")
-  if shareRef == "" {
-    auth.SendUnauthorized(w)
-    return
-  }
-
-  b, err := h.bs.Db.Get(shareRef)
-  if err != nil {
-    auth.SendUnauthorized(w)
-    return
-  }
-
-  share := &blob.Share{}
-  err = blob.Unmarshal(b, share)
-  util.Check(err)
-
-  ref := path.Base(req.URL.Path)
-  b, err = h.bs.Db.Get(ref)
-  util.Check(err)
-
-  if !share.AuthorizedGet(b) {
-    auth.SendUnauthorized(w)
-    return
-  }
-
-  w.Header().Set(ActionStatus, ActionSuccess)
-  w.Write(b.Content())
-  fmt.Println("successful retrieval")
+	auth.SendUnauthorized(w)
 }
 
 type putHandler struct {
-  bs *Server
+	bs *Server
 }
 
 func (h *putHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-  defer func() {
-    if r := recover(); r != nil {
-      w.Header().Set(ActionStatus, ActionFailed)
-      fmt.Println("blob post issues: ", r)
-    }
-  }()
-
-  body, err := ioutil.ReadAll(req.Body)
-  util.Check(err)
-
-  b := blob.NewRaw(body)
-  err = h.bs.Db.Put(b)
-  if err == blobdb.HashCollideErr {
-    w.Header().Set(ActionStatus, ActionFailed)
-    return
-  }
-  h.bs.notify(b)
-
-  w.Header().Set(ActionStatus, ActionSuccess)
-  fmt.Println("successful post")
+	data, err := ioutil.ReadAll(req.Body)
+	ref, n, err := h.bs.Db.Put(bytes.NewBuffer(data))
+	h.bs.notifyListeners(data)
 }
 
-// Unauthorized checks for and handles cases where authentication can occur via
-// share blobs.
 func (h *putHandler) Unauthorized(w http.ResponseWriter, req *http.Request) {
-  auth.SendUnauthorized(w)
+	auth.SendUnauthorized(w)
 }
 
-type indexHandler struct {
-  bs *Server
+type enumHandler struct {
+	bs *Server
 }
 
-func (h *indexHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-  defer func() {
-    if r := recover(); r != nil {
-      w.Header().Set(ActionStatus, ActionFailed)
-      fmt.Println("index access failed: ", r)
-    }
-  }()
-
-  name := req.Header.Get(IndexField)
-  ind, ok := h.bs.inds[name]
-  if !ok {
-    panic("invalid index name")
-  }
-
-  it, err := ind.GetIter(req)
-  util.Check(err)
-
-  n, err := strconv.Atoi(req.Header.Get(ResultCountField))
-  util.Check(err)
-
-  refs := multipart.NewWriter(w)
-  w.Header().Set("Content-Type", "multipart/mixed")
-  w.Header().Set(BoundaryField, refs.Boundary() )
-
-  defer refs.Close()
-  for i := 0; i < n; i++ {
-    ref, err := it.Next()
-    if err != nil {
-      break
-    }
-
-    b, err := h.bs.Db.Get(ref)
-    util.Check(err)
-
-    part, err := refs.CreateFormFile("blob-ref", ref)
-    util.Check(err)
-    part.Write(b.Content())
-  }
+func (h *enumHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 }
 
-func (h *indexHandler) Unauthorized(w http.ResponseWriter, req *http.Request) {
-  auth.SendUnauthorized(w)
+func (h *enumHandler) Unauthorized(w http.ResponseWriter, req *http.Request) {
+	auth.SendUnauthorized(w)
 }
-
